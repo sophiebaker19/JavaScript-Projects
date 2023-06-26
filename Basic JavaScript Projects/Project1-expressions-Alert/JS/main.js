7ec73d7608af87d25c7bc4dd4e2f9b066c1dfb46
sophiebaker19 <sophiebaker2004@gmail.com>
2023-06-26 16:58:46 +0100

    Basic JavaScript Project
    Assignments learning basic JavaScript instructions.

---
+++ Basic JavaScript Projects/Project1-expressions-Alert/JS/main.js
@@ -0,0 +1,23 @@
+var A = "This is a string, "; //this is the variable named A
+document.write (A); // this executes the variable with its corrosponding text
+                    // as a line of text on the page
+
+var B = "This is an alert"; //this is the variable named B
+window.alert(B); // this executes the variable with its corrosponding text
+                 // as an alert as a pop up
+
+document.write ("this isn\'t an alert box! "); //this is an escape character
+
+document.write ("\"What separates design from art" //these lines are concatenating a 
++" is that art is meant to be functional\"" //string
++"-Cameron Moll.")
+
+var C = " This is the beginning of the string"
+var D = " this is the end of the string. ";
+document.write (C+D); // these lines execute assigned variables in a concatenated string
+
+var Car1 = "Volvo", Car2 = "BMW", Car3 = "Mercedes", 
+Car4 = "Toyota", Car5 = "Ford";
+document.write(Car5) // these lines have multiple variables
+
+document.write(5+5); //this is an expression
