8057a3e0ce902a8af1291e6c2b9ea6de20cf4b53
sophiebaker19 <sophiebaker2004@gmail.com>
2023-06-27 16:38:36 +0100

    Changing Comment
    Changing 'message' in the comments to 'variable'

--- Basic JavaScript Projects/Project2-functions/JS/main.js
+++ Basic JavaScript Projects/Project2-functions/JS/main.js
@@ -1,8 +1,8 @@
 function First_function() { //calls the function 
     var message = "HI!";    //variable that displays a message
-    document.getElementById("para").innerHTML = message; //tells the computer to display the message in place of the paragraph
-    var goback = "Go Back"; // variablethat display a message on the button
-    document.getElementById("button").innerHTML = goback; //tells the computer to diplay the message on the button
+    document.getElementById("para").innerHTML = message; //tells the computer to display the variable in place of the paragraph
+    var goback = "Go Back"; // variable that display a message on the button
+    document.getElementById("button").innerHTML = goback; //tells the computer to diplay the variable on the button
 }
 
 function Second_function() { //calls the function
